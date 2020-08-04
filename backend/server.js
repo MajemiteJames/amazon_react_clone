486bf32f117011e8d5c0e1035bdddbb2946165c4
MajemiteJames <majemiteokoro@gmail.com>
2020-08-04 17:59:31 +0100

    implementd the managing of product to update and delete and the admin page to addd

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import data from "./data";
 import dotenv from "dotenv";
 const bodyParser = require("body-parser");
 import userRoute from "./routes/userRoute";
+import productRoute from "./routes/productRoute";
 const connectDB = require("./config/db");
 
 dotenv.config();
@@ -16,17 +17,18 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
-app.get("/api/products/:id", (req, res) => {
-  const productId = req.params.id;
-  const product = data.products.find((x) => x._id === productId);
-  if (product) res.send(product);
-  else res.status(404).send({ msg: "Product Not Found." });
-});
+// app.get("/api/products/:id", (req, res) => {
+//   const productId = req.params.id;
+//   const product = data.products.find((x) => x._id === productId);
+//   if (product) res.send(product);
+//   else res.status(404).send({ msg: "Product Not Found." });
+// });
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
+// app.get("/api/products", (req, res) => {
+//   res.send(data.products);
+// });
 
 app.listen(5000, () => {
   console.log("Server started at http://localhost:5000");
